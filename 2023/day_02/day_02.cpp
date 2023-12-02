e1f78331ce2d318fbfb7a995dcd0d0dc0ab10280
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2023-12-02 08:35:19 +0000

    2023 day 2

--- 2023/day_02/day_02.cpp
+++ 2023/day_02/day_02.cpp
@@ -1,20 +1,60 @@
 #include "day_02.h"
 
 void day_02::print_answers() {
-	std::cout << "Not implemented - Insert day name here" << std::endl; 
+	std::cout << "--- Day 2: Cube Conundrum ---" << std::endl; 
 
-	std::cout << "Part 1: " << part_one<int>("input.txt") << '\n';
-	std::cout << "Part 2: " << part_two<int>("input.txt") << '\n';
+	std::vector<std::string> input = parser::get_lines(INPUT_FILE);
+
+	std::vector<Game> game_list;
+
+	for (const std::string& s : input) {
+		game_list.push_back(to_game(s));
+	}
+
+	std::cout << "Part 1: " << part_one(game_list) << '\n';
+	std::cout << "Part 2: " << part_two(game_list) << '\n';
 }
 
-template<typename T>
-T part_one(const std::string& input_file) {
-	T answer{};
-	return answer;
+int day_02::part_one(const std::vector<Game>& game_list) {
+	// convert list of strings to list of games
+	// ensure sufficient space is reserved for all games to avoid reallocation
+	auto total_score = [](int i, const Game& g) -> int {
+		if (g.is_possible()) {
+			return i + g.id;
+		}
+		return i;
+		};
+
+	return std::accumulate(game_list.begin(), game_list.end(), 0, total_score);
 }
 
-template<typename T>
-T part_two(const std::string& input_file) {
-	T answer{};
-	return answer;
+int day_02::part_two(const std::vector<Game>& game_list) {
+	auto total_score = [](int i, const Game& g) -> int {
+		return i + g.cube_power();
+		};
+
+	return std::accumulate(game_list.begin(), game_list.end(), 0, total_score);
+}
+
+Game day_02::to_game(const std::string& text) {
+	Game game;
+
+	auto max_int = [&](std::regex rgx) -> int {
+		int max{};
+		std::smatch match;
+		std::string temp(text);
+		while (std::regex_search(temp, match, rgx)) {
+			int n = std::stoi(match[1].str());
+			max = n > max ? n : max;
+			temp = match.suffix();
+		}
+		return max;
+	};
+
+	game.id = max_int(std::regex("Game (\\d+)"));
+	game.max_red = max_int(std::regex("(\\d+) r"));
+	game.max_green = max_int(std::regex("(\\d+) g"));
+	game.max_blue = max_int(std::regex("(\\d+) b"));
+
+	return game;
 }
--- 2023/day_02/day_02.h
+++ 2023/day_02/day_02.h
@@ -1,15 +1,33 @@
 #pragma once
 
 #include <iostream>
+#include <numeric>
+#include <regex>
+#include <string>
+#include <vector>
+
+#include <parser.h>
+
+struct Game {
+	int id{}, max_red{}, max_blue{}, max_green{};
+
+	bool is_possible() const {
+		return max_red < 13 && max_green < 14 && max_blue < 15;
+	}
+
+	int cube_power() const {
+		return max_red * max_green * max_blue;
+	}
+};
+
+const std::string INPUT_FILE{ "C:\\Users\\jonat\\source\\repos\\AdventOfCode\\2023\\day_02\\input.txt" };
+const std::string TEST_FILE{ "C:\\Users\\jonat\\source\\repos\\AdventOfCode\\2023\\day_02\\test.txt" };
 
 
 namespace day_02 {
 	void print_answers();
+	int part_one(const std::vector<Game>& input);
+	int part_two(const std::vector<Game>& input);
+	Game to_game(const std::string& text);
 }
-
-
-template<typename T>
-T part_one(const std::string& input_file);
-
-template<typename T>
-T part_two(const std::string& input_file);
+ 
