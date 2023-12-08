31ae28fd1d7f81fc2f3888a3a91a797db2158d14
Jonathan WEBSTER <64645223+JWR26@users.noreply.github.com>
2023-12-08 14:02:16 +0000

    2019 test project created

---
+++ _tests/test_2019/test_day_01.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace test2019
+{
+	TEST_CLASS(test2019)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
